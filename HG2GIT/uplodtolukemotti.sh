a2463f255212bd9b7d83fbf8d30d7ad15c5b12cd
jariperttunen <32894586+jariperttunen@users.noreply.github.com>
2021-10-23 14:34:44 +0300

    Update uplodtolukemotti.sh

--- HG2GIT/uplodtolukemotti.sh
+++ HG2GIT/uplodtolukemotti.sh
@@ -5,10 +5,12 @@ for dirname in $dirnames
 do
     echo $dirname
     pushd $dirname
-    git remote rename origin master
+    #Assuming cloned repository is origin 
+    git remote rename origin clonedorigin
     repository=lukemotti/$dirname
     echo $repository
     gh repo create $repository --private --confirm
+    #GitHub is origin by default, assuming the branch name is master
     git push origin master
     popd
     #exit
